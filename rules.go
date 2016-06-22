7a70a537d66f9925a71fea737776a6fc7aa85ed4
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2016-06-23 00:03:54 +0300

    Add draft of game loop

--- rules.go
+++ rules.go
@@ -3,7 +3,8 @@ package main
 // Player is an interface of players of a card game
 type Player interface {
 	takeCard(card Card)
-	placeCard(topCard Card) Card
+	placeCard(topCard Card) *Card // TODO: It should be possible to place up to 4 cards
+	handIsEmpty() bool
 }
 
 // CzechDurakGame is rules of the game
@@ -21,4 +22,26 @@ func NewCzechDurakGame(player1, player2 *Player) *CzechDurakGame {
 	game.player1 = player1
 	game.player2 = player2
 	return game
+}
+
+func (game *CzechDurakGame) play() {
+	openCards := make([]Card, 36)
+	openCards = append(openCards, game.deck.dealCard())
+	players := [2]Player{*game.player1, *game.player2}
+	move := 0
+	for {
+		player := players[move%2]
+		card := player.placeCard(openCards[len(openCards)-1])
+		if card == nil {
+			player.takeCard(game.deck.dealCard()) // TODO: check if deck empty
+		} else {
+			// TODO: check is card right
+			openCards = append(openCards, *card)
+		}
+		if player.handIsEmpty() {
+			break
+			// TODO: do something about winner
+		}
+		move++
+	}
 }
